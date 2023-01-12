3f29eb09f5aef7487163db05eff56a0b4bf92b50
JingYee-JY <jingyeetan2005@gmail.com>
2023-01-12 10:11:52 +0800

    Update UI

--- script.js
+++ script.js
@@ -1,99 +1,123 @@
-const start = document.querySelector(".start");
-const startButton = document.querySelector(".startButton");
-const game = document.querySelector(".game");
-const ans1 = document.getElementById("btn1");
-const ans2 = document.getElementById("btn2");
-const ans3 = document.getElementById("btn3");
-const ans4 = document.getElementById("btn4");
-const timerCount = document.querySelector(".timer-count");
+const startButton = document.querySelector(".start");
+const began = document.querySelector(".startGame");
+const playAgain = document.querySelector(".again");
+const homeButton = document.querySelector(".home");
+
+const start = document.querySelector(".startPage");
+const instruction = document.querySelector(".instructionPage");
+const game = document.querySelector(".gamePage");
+const final = document.querySelector(".finalPage");
+
+const choiceButtons = document.querySelectorAll(".choiceButton");
 const scoreCount = document.querySelector(".score-count");
-const popUp = document.querySelector(".popUp");
-const picture = document.querySelector(".picture");
-const inform = document.querySelector(".inform");
-const final = document.querySelector(".final");
-const wellDone = document.querySelector(".wellDone");
-const playAgain = document.querySelectorAll(".playAgain");
-const homeButton = document.querySelectorAll(".home");
-const resultScoreCount = document.querySelectorAll(".result-score-count");
+const questionNumber = document.querySelector(".question-count");
+const text = document.querySelector(".text");
+const medal = document.querySelector(".medal")
+const words = document.querySelector(".words2")
+const scoreText = document.querySelector(".scoreText")
 
 const clickSound = document.getElementById("click")
 const completed = document.getElementById("completed")
 const clap = document.getElementById("clap")
 
+
 let startGame;
 let time;
 
-let score = 0;
-let correctAnswer;
-let pattern;
-let choice;
-let choicePattern;
-let choosenAnswer
-let tempoArray = [];
+//CHANGE TOTAL QUESTION HERE
+let totalQuestion = 5;
 
+let currentQuestion;
+let score = 0;
+let answeredCorrect = 0;
+let correctBtn;
+let correctAnswer;
+let choosenAnswer;
+
+//CHANGE HERE FOR ALL THE IMAGES CARDS
 const character =[
-    {number:"1", image:"./img/Fish Seller_ Mrs. Fillet.png"},
-    {number:"2", image:"./img/Gardener_ Mr. Weeder.png"},
-    {number:"3", image:"./img/Policemen_ Polly Plod.png"},
-    {number:"4", image:"./img/Fish Seller_ Mr. Fillet.png"},
-    {number:"5", image:"./img/Gardener_ Wendy Weeder.png"},
-    {number:"6", image:"./img/Policemen_ Percy Plod.png"},
-    {number:"7", image:"./img/Fish Seller_ Freddy Fillet.png"},
-    {number:"8", image:"./img/Gardener_ Mrs. Weeder.png"},
-    {number:"9", image:"./img/Policemen_Mrs. Plod.png"},
-    {number:"10", image:"./img/Fish Seller_ Flossie Fillet.png"},
-    {number:"11", image:"./img/Gardener_ Willy Weeder.png"},
-    {number:"12", image:"./img/Policemen_Mr. Plod.png"}
+    {number:"1", image:"./img/family1Father.png"},
+    {number:"2", image:"./img/family1Mother.png"},
+    {number:"3", image:"./img/family1Daughter.png"},
+    {number:"4", image:"./img/family1Son.png"},
+
+    {number:"5", image:"./img/family2Father.png"},
+    {number:"6", image:"./img/family2Mother.png"},
+    {number:"7", image:"./img/family2Daughter.png"},
+    {number:"8", image:"./img/family2Son.png"},
+
+    {number:"9", image:"./img/family3Father.png"},
+    {number:"10", image:"./img/family3Mother.png"},
+    {number:"11", image:"./img/family3Daughter.png"},
+    {number:"12", image:"./img/family3Son.png"}
 ];
+
+//last number of the group
+const group = ["4","8","12"]
+
+//CHANGE HERE FOR THE NUMBER OF CARDS PER GROUP TO REMOVE SAME NUMBER OF CARDs AND SAME FOR BUTTONS
+const numberOfCardsInAGroup = 4;
+const numberOfButtons = 4;
 
 startButton.addEventListener("click", () => {
     playClickSound()
     let delay = setTimeout(() => {
         start.classList.add("hide")
+        instruction.classList.remove("hide")
+        currentQuestion = 0;
+    }, 200);
+})
+
+began.addEventListener("click", () => {
+    playClickSound()
+    let delay = setTimeout(() => {
+        instruction.classList.add("hide")
         game.classList.remove("hide")
         startGame = true
-        time = 30
-        timerCount.innerHTML = `${time} s`;
         score = 0
+        answeredCorrect = 0
         Question()
     }, 200);
 })
 
-for(let y=1; y < 5; y++){
-    let currentBtnClass = "btn" + y
+playAgain.addEventListener("click", () => {
+    playClickSound()
+    let delay = setTimeout(() => {
+        final.classList.add("hide")
+        start.classList.remove("hide")
+    }, 200);
+})
 
-    let currentBtn = document.querySelector(`.${currentBtnClass}`)
+//event listener for the all button
+choiceButtons.forEach(function(currentBtn){
     currentBtn.addEventListener("click", () => {
-        let data = currentBtn.getAttribute("data")
-        startGame = false
-        playClickSound()
-        popUp.classList.remove("hide")
-        if(data == correctAnswer){
-            score += 10
-            picture.src = "./img/correct.png"
-            inform.innerHTML = "That's correct!"
+        if(!choosenAnswer){
+            choosenAnswer = true
+            let data = currentBtn.getAttribute("data")
+            playClickSound()
+            if(data == correctAnswer){
+                score += 10
+                answeredCorrect += 1
+                scoreCount.innerHTML = `<b>${score}</b>`
+                console.log(score)
+                text.textContent = "Correct!"
+                currentBtn.classList.add("correct")
+            }
+            if(data != correctAnswer){
+                text.textContent = "Wrong!"
+                currentBtn.classList.add("wrong")
+                correctBtn.classList.add("correct")
+            }
+            let delay = setTimeout(() => {
+                text.textContent = ""
+                document.querySelector(".correct").classList.remove("correct")
+                if(document.querySelector(".wrong") != null){
+                    document.querySelector(".wrong").classList.remove("wrong")
+                }
+                startGame = true
+                Question()
+            }, 1500);
         }
-        if(data != correctAnswer){
-            picture.src = "./img/wrong.png"
-            inform.innerHTML = "Try again!"
-        }
-        let delay = setTimeout(() => {
-            popUp.classList.add("hide")
-            startGame = true
-            Question()
-          }, 1500);
-    })
-
-}
-
-playAgain.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        playClickSound()
-        let delay = setTimeout(() => {
-            wellDone.classList.add("hide")
-            final.classList.add("hide")
-            start.classList.remove("hide")
-        }, 200);
     })
 })
 
@@ -106,76 +130,99 @@ homeButton.forEach((btn) => {
     })
 })
 
-function updateCountDown(){
-    if(startGame == true){
-        timerCount.innerHTML = `${time} s`;
-        scoreCount.innerHTML = `${score} pt`;
-        if(time == 0){
-            startGame = false
-            if(score >= 50){
-                clap.currentTime = 0
-                clap.play()
-                game.classList.add("hide")
-                wellDone.classList.remove("hide")
-                resultScoreCount.forEach(function(item){
-                    item.innerHTML = `${score}`
-                })
-                return
-            }
-            else{
-                completed.currentTime = 0
-                completed.play()
-                game.classList.add("hide")
-                final.classList.remove("hide")
-                resultScoreCount.forEach(function(item){
-                    item.innerHTML = `${score}`
-                })
-                return
-            }
-        }
-        time--;
-    }
-}
-
-function updateScore(){
-    if(startGame == true){
-        scoreCount.innerHTML = `${score} pt`;
-    }
-}
-
 function Question(){
 
-    if(tempoArray.length < 2){
-        console.log("D")
-        for(let i = 0; i < character.length; i++){
-            tempoArray.push(character[i])
+    if(currentQuestion == totalQuestion){
+        game.classList.add("hide")
+        final.classList.remove("hide")
+        let starScore = totalQuestion / 5;
+        let pass = totalQuestion / 2
+        if(answeredCorrect == totalQuestion){
+            clap.currentTime = 0
+            clap.play()
+            scoreText.textContent = "Excellent!"
+            medal.src = "./img/Excellent.png"
         }
+        else if(answeredCorrect >= pass){
+            completed.currentTime = 0
+            completed.play()
+            scoreText.textContent = "Well done!"
+            if(answeredCorrect < totalQuestion && answeredCorrect >= totalQuestion - starScore) // score < total && score >= 8
+                medal.src = "./img/Well Done.png"
+            else if(answeredCorrect < totalQuestion - starScore && answeredCorrect >= (totalQuestion - starScore - starScore)) // score < 8 && score >= 6
+                medal.src = "./img/Well Done1.png"
+            else
+                medal.src = "./img/You Tried.png"
+        }
+        else if(answeredCorrect < pass){
+            clap.currentTime = 0
+            clap.play()
+            scoreText.textContent = "You tried!"
+            if(answeredCorrect == starScore + starScore)
+                medal.src = "./img/You Tried.png"
+            else if(answeredCorrect < starScore + starScore && score >= starScore) // score < 4 && score >= 2
+                medal.src = "./img/You Tried1.png"
+            else
+                medal.src = "./img/You Tried2.png"
+        }
+        words.innerHTML = answeredCorrect + "/" + totalQuestion
+        return
     }
+
+    currentQuestion += 1;
+    questionNumber.innerHTML = currentQuestion + "/" + totalQuestion
+
+    let tempoArray = []
+
+    for(let i = 0; i < character.length; i++){
+        tempoArray.push(character[i])
+    }
+
+    let correctAnswerArray = [];
+    let buttonsArray = [];
+
+    //get random group
+    let randomQuestionGroup = Math.floor(Math.random() * group.length)
+
+    //remove correct answer
+    for(let i = 0; i < numberOfCardsInAGroup; i++){
+        let removeCurrent = parseInt(group[randomQuestionGroup]) - i - 1
+        correctAnswerArray.push(tempoArray[removeCurrent])
+        tempoArray.splice(removeCurrent, 1)
+    }
+
     choosenAnswer = false
-    let randomQuestionIndex = Math.floor(Math.random() * tempoArray.length);
 
-    for(let y = 1; y < 5; y++){
-        let currentClass = "btn" + y;
-        let currentBtn = document.querySelector(`.${currentClass}`)
-        currentBtn.src = tempoArray[randomQuestionIndex].image
-        currentBtn.setAttribute("data", tempoArray[randomQuestionIndex].number)
+    //give all wrong answer
+    choiceButtons.forEach(function(item){
+        let randomQuestionIndex = Math.floor(Math.random() * tempoArray.length);
+        item.src = tempoArray[randomQuestionIndex].image
+        item.setAttribute("data", tempoArray[randomQuestionIndex].number)
+        tempoArray.splice(randomQuestionIndex, 1);
+        buttonsArray.push(item)
+    })
+
+    console.log(buttonsArray)
+
+    for(let i = 1; i < numberOfButtons; i++){
+        let randomBtn = Math.floor(Math.random() * buttonsArray.length)
+        let randomCorrectAnswer = Math.floor(Math.random() * correctAnswerArray.length)
+
+        let wrongBtn = buttonsArray[randomBtn];
+        let wrongAnswer = correctAnswerArray[randomCorrectAnswer]
+
+        wrongBtn.src = wrongAnswer.image
+        wrongBtn.setAttribute("data", wrongAnswer.number)
+
+        buttonsArray.splice(randomBtn, 1);
+        correctAnswerArray.splice(randomCorrectAnswer, 1);
+
     }
+    correctBtn = buttonsArray[0];
+    correctAnswer = correctBtn.getAttribute("data")
 
-    tempoArray.splice(randomQuestionIndex, 1);
-    console.log(tempoArray)
-    
-    let correctAnswerIndex = Math.floor(Math.random() * tempoArray.length);
-    console.log(correctAnswerIndex)
-    let randomclass = "btn" + (Math.floor(Math.random() * 4) + 1);
-    let randomBtn = document.querySelector(`.${randomclass}`)
-    randomBtn.src = tempoArray[correctAnswerIndex].image
-    correctAnswer = tempoArray[correctAnswerIndex].number
-    randomBtn.setAttribute("data", correctAnswer)
-    tempoArray.splice(correctAnswerIndex, 1)
+    console.log(correctAnswer)
 }
-
-setInterval(updateCountDown, 1000)
-setInterval(updateScore, 1)
 
 function playClickSound(){
     console.log(clickSound)
